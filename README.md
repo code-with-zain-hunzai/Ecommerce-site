9cad3456c3473e3ce83fed126ca6836825553b20
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2024-07-18 11:38:07 +0500

    update files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Ecommerce-site on Next.js 
 
-In this code i will provide you Front-end and Backend code
+In this code i will provide you Front-end and Backend code 
--- src/components/admin-panel/Loader.tsx
+++ src/components/admin-panel/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const loader = () => {
     return (
-        <div className='fixed w-full h-screen top-0 left-0  grid place-items-center'>
+        <div className='fixed w-full h-screen bg-[#f305] top-0 left-0 grid place-items-center'>
             <span className="loader"></span>
         </div>
     )
